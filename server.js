88d24839fc258e781563bf1938dd6190d493b4de
Adam Bouhadid <122629879+AdamBhd@users.noreply.github.com>
2025-03-25 11:35:52 -0400

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ function start(route, handle) {
         route(handle, pathname, response, request);
     }
 
-    http.createServer(onRequest).listen(8080);
+    http.createServer(onRequest).listen(8080, '0.0.0.0');
+
 }
 
 exports.start = start;
